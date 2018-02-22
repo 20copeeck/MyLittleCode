8b5cc48f9e7d04b5e8bb17213822f24931361cae
chelovecheki <36590480+chelovecheki@users.noreply.github.com>
2018-02-22 16:56:54 +0700

    Переименованы функции

---
+++ ProgrammingPractics/lab1.h
@@ -0,0 +1,15 @@
+#pragma once
+
+void Initialization();
+void Division();
+void Amount();
+void Pointer();
+void Parity();
+void Multiplicity();
+void Cycle–recondition();
+void Cycle–ostcondition();
+void CycleIterative();
+void Breaker();
+void TypeConversions();
+
+void LauncherLab1();
